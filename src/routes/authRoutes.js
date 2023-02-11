94051ba1790db27c6a5a5cfda2c9992a110b27ea
Mahmoud-Nwiry <mamo992001@gmail.com>
2023-02-12 01:00:38 +0200

    add lazy to pages

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,16 +1,19 @@
-import Login from '../pages/Login'
-import Signup from '../pages/Signup';
+import { lazy, Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
+
+
+const Login = lazy(()=> import('../pages/Login'))
+const Signup = lazy(()=> import('../pages/Signup'))
 
 export const authRouts = {
     path : '/auth',
     element : <Outlet />,
     children :[{
         path : 'login',
-        element : <Login />,
+        element : <Suspense fallback="Loading..."><Login /></Suspense>,
     },
     {
         path : 'signup',
-        element : <Signup />,
+        element : <Suspense fallback="Loading..."><Signup /></Suspense>,
     }]
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,34 +1,49 @@
+import { lazy, Suspense } from 'react';
+import { authRouts } from './authRoutes'
+
+
 import MainLayout from '../layouts/MainLayout'
-import Cart from '../pages/Cart';
-
-import Home from '../pages/Home';
-import Products from '../pages/Products';
-import ProductsDetails from '../pages/ProductsDetails';
 
 
-import {authRouts} from './authRoutes'
+
+const Home = lazy(() => import('../pages/Home'))
+const Products = lazy(() => import('../pages/Products'))
+const ProductsDetails = lazy(() => import('../pages/ProductsDetails'))
+const Cart = lazy(() => import('../pages/Cart'))
 
 export const routes = [
-    {
-      index : true,
-      element : <MainLayout><Home /></MainLayout>
-    },
-    {
-      path : '/products',
-      element : <MainLayout />,
-      children : [
-        {
-          index : true,
-          element : <Products />
-        },{
-          path : ':id',
-          element : <ProductsDetails />
-        }
-      ]
-    },
-    {
-      path : '/cart',
-      element : <Cart />
-    },
-    authRouts
+  {
+    index: true,
+    element: <Suspense fallback="Loading...">
+      <MainLayout>
+        <Home />
+      </MainLayout>
+    </Suspense>
+  },
+  {
+    path: '/products',
+    element: <Suspense fallback="Loading...">
+                <MainLayout />
+            </Suspense>,
+    children: [
+      {
+        index: true,
+        element: <Suspense fallback="Loading...">
+                  <Products />
+                </Suspense>
+      }, {
+        path: ':id',
+        element: <Suspense fallback="Loading...">
+                  <ProductsDetails />
+                  </Suspense>
+      }
+    ]
+  },
+  {
+    path: '/cart',
+    element: <Suspense fallback="Loading...">
+              <Cart />
+            </Suspense>
+  },
+  authRouts
 ];
